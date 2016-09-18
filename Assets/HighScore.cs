b69bc3ab132105e9eceb7472c6f619e37df19098
dc820 <dc820@scarletmail.rutgers.edu>
2016-09-18 12:15:30 -0400

    Uncommented HighScore Saver/ Baskets Resetting When 0, Prototype Complete

--- Assets/HighScore.cs
+++ Assets/HighScore.cs
@@ -5,25 +5,25 @@ using UnityEngine.UI;
 public class HighScore : MonoBehaviour {
     static public int score = 1000;
     
-    //void Awake()
-    //{
+    void Awake()
+    {
         //If the ApplePickerHighScore already exists, read it
-      //  if (PlayerPrefs.HasKey("ApplePickerHighScore"))
-        //{
-          //  score = PlayerPrefs.GetInt("ApplePickerHighScore");
-       // }
-        // Assign the high score to ApplePickerHighScore
-        //PlayerPrefs.SetInt("ApplePickerHighScore", score);
-   // }
+        if (PlayerPrefs.HasKey("ApplePickerHighScore"))
+        {
+            score = PlayerPrefs.GetInt("ApplePickerHighScore");
+        }
+         //Assign the high score to ApplePickerHighScore
+        PlayerPrefs.SetInt("ApplePickerHighScore", score);
+    }
 
     void Update ()
     {
         Text gt = this.GetComponent<Text>();
         gt.text = "High Score: " + score;
         //Update ApplePickerHighScore in PlayerPrefs if necessary
-       // if (score > PlayerPrefs.GetInt("ApplePickerHighScore"))
-        //{
-         //   PlayerPrefs.SetInt("ApplePickerHighScore", score);
-        //}
+        if (score > PlayerPrefs.GetInt("ApplePickerHighScore"))
+        {
+            PlayerPrefs.SetInt("ApplePickerHighScore", score);
+        }
     }
 }
