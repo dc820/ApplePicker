7cf255919aa53122ab251b1eadb2c9a9aeb5d8d2
dc820 <dc820@scarletmail.rutgers.edu>
2016-09-18 00:53:49 -0400

    Apple Drop Notify/ Destorying Basket Script

--- Assets/ApplePicker.cs
+++ Assets/ApplePicker.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class ApplePicker : MonoBehaviour
 {
@@ -7,15 +8,36 @@ public class ApplePicker : MonoBehaviour
     public int numBaskets = 3;
     public float basketBottomY = -14f;
     public float basketSpacingY = 2f;
+    public List<GameObject> basketList;
     // Use this for initialization
     void Start()
     {
+        basketList = new List<GameObject>();
         for (int i = 0; i < numBaskets; i++)
         {
             GameObject tBasketGO = Instantiate(basketPrefab) as GameObject;
             Vector3 pos = Vector3.zero;
             pos.y = basketBottomY + (basketSpacingY * i);
             tBasketGO.transform.position = pos;
+            basketList.Add(tBasketGO);
         }
+    }
+
+    public void AppleDestroyed()
+    {
+        // Destroy all of the falling Apples
+        GameObject[] tAppleArray = GameObject.FindGameObjectsWithTag("Apple");
+        foreach (GameObject tGO in tAppleArray)
+        {
+            Destroy(tGO);
+        }
+        //Destroy one of the Baskets
+        //Get the index of the last Basket in basketList
+        int basketIndex = basketList.Count - 1;
+        // Get a reference to that Basket GameObject
+        GameObject tBasketGO = basketList[basketIndex];
+        // Remove the Basket from the List and destory the GameObject
+        basketList.RemoveAt(basketIndex);
+        Destroy(tBasketGO);
     }
 }
