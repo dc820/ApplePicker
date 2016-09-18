f46c436307218100568301c4ab2ac9f27da69104
dc820 <dc820@scarletmail.rutgers.edu>
2016-09-18 01:45:15 -0400

    Commented out HighScore Save, Exception Basket Line 32

--- Assets/ApplePicker.cs
+++ Assets/ApplePicker.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.SceneManagement;
+//using UnityEngine.SceneManagement;
 
 public class ApplePicker : MonoBehaviour
 {
@@ -9,18 +9,18 @@ public class ApplePicker : MonoBehaviour
     public int numBaskets = 3;
     public float basketBottomY = -14f;
     public float basketSpacingY = 2f;
-    public List<GameObject> basketList;
+    //public List<GameObject> basketList;
     // Use this for initialization
     void Start()
     {
-        basketList = new List<GameObject>();
+        //basketList = new List<GameObject>();
         for (int i = 0; i < numBaskets; i++)
         {
             GameObject tBasketGO = Instantiate(basketPrefab) as GameObject;
             Vector3 pos = Vector3.zero;
             pos.y = basketBottomY + (basketSpacingY * i);
             tBasketGO.transform.position = pos;
-            basketList.Add(tBasketGO);
+            //basketList.Add(tBasketGO);
         }
     }
 
@@ -34,17 +34,17 @@ public class ApplePicker : MonoBehaviour
         }
         //Destroy one of the Baskets
         //Get the index of the last Basket in basketList
-        int basketIndex = basketList.Count - 1;
+        //int basketIndex = basketList.Count - 1;
         // Get a reference to that Basket GameObject
-        GameObject tBasketGO = basketList[basketIndex];
+        //GameObject tBasketGO = basketList[basketIndex];
         // Remove the Basket from the List and destory the GameObject
-        basketList.RemoveAt(basketIndex);
-        Destroy(tBasketGO);
+        //basketList.RemoveAt(basketIndex);
+        //Destroy(tBasketGO);
 
         // Restart the game, which doesn't affect HighScore.Score
-        if(basketList.Count == 0)
-        {
-            SceneManager.LoadScene("_Scene_0");
-        }
+        //if(basketList.Count == 0)
+        //{
+        //    SceneManager.LoadScene("_Scene_0");
+        //}
     }
 }
